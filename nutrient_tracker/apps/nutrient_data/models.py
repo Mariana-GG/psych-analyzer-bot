e1c548d87124f19fd0a6fc2950272d16beeb4864
Mariana-GG <aurea.mariana.gg@gmail.com>
2025-07-26 15:05:37 +0200

    Initialize k4tsuya-patch-3

---
+++ nutrient_tracker/apps/nutrient_data/models.py
@@ -0,0 +1,37 @@
+"""Django REST Framework models for nutrient data."""
+
+from django.db import models
+from apps.nutrient_data.enums import Category, Subcategory
+
+
+# Create your models here.
+class Nutrient(models.Model):
+    food_group = models.CharField(max_length=100, blank=False)
+    food_name = models.CharField(max_length=100, unique=True, blank=False)
+    is_favorite = models.BooleanField(
+        default=False
+    )  # needs to be handled differently
+    # is_micronutrient determines if its macro or micro nutrients
+    category = models.CharField(  # macro, micro
+        max_length=100,
+        choices=Category.get_choices(),
+        default=Category.get_default_category(),
+    )
+    subcategory = models.CharField(  # vitamin, mineral, carbs, etc.
+        max_length=100,
+        choices=Subcategory.get_choices(),
+        default=Subcategory.get_default_subcategory(),
+    )
+    measure_unit = models.CharField(max_length=2)  # g, mg, µg
+
+    class Meta:
+        verbose_name = "Nutrient"
+        verbose_name_plural = "Nutrients"
+
+    def __str__(self):
+        return self.food_name
+
+
+# turn this into a rest API
+# add categories for the different forms of nutrients
+# add nutrient value from given databank
